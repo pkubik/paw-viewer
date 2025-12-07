a96ae2588712626815b7f935dd4e486dd0867f1d
Pawel Kubik <p.kubik.pl@gmail.com>
2025-12-07 13:42:36 +0100

    Add fill

--- src/paw_viewer/shaders/slider_fragment.glsl
+++ src/paw_viewer/shaders/slider_fragment.glsl
@@ -14,20 +14,35 @@ uniform Slider
     float y;
 } slider;
 
-void main()
+float verticalBarSDF(vec2 v1, vec2 v2)
 {
-    vec2 v1 = vec2(slider.start_x, slider.y);
-    vec2 v2 = vec2(slider.end_x, slider.y);
-
     float d = abs(gl_FragCoord.y - slider.y);
     if (gl_FragCoord.x < v1.x || gl_FragCoord.x > v2.x) {
         float d1 = length(gl_FragCoord.xy - v1);
         float d2 = length(gl_FragCoord.xy - v2);
         d = min(d1, d2);
     }
+    return d;
+}
+
+void main()
+{
+    float d = verticalBarSDF(
+        vec2(slider.start_x, slider.y),
+        vec2(slider.end_x, slider.y)
+    );
+
+    float pd = verticalBarSDF(
+        vec2(slider.start_x, slider.y),
+        vec2(slider.knob_x, slider.y)
+    );
 
     float m1 = 1.0 - smoothstep(5., 10., d);
+    float mfill = 1.0 - smoothstep(4., 5., pd);
     float m2 = 1.0 - smoothstep(4., 5., d);
     m1 = clamp(m1 - m2, 0., 1.);
-    final_color = mix(vec4(0), vec4(0.9, 0.9, 0.9, 0.9), m1) + mix(vec4(0), vec4(0.2, 0.5, 0.3, 0.8), m2);
+    m2 = clamp(m2 - mfill, 0., 1.);
+    final_color = mix(vec4(0), vec4(0.9, 0.9, 0.9, 0.9), m1)
+        + mix(vec4(0), vec4(0.1, 0.3, 0.15, 0.5), m2)
+        + mix(vec4(0), vec4(0.2, 0.5, 0.3, 0.9), mfill);
 }
