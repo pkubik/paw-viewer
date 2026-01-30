9652871c4dd8bc99c4bcb2e428a8a884ec645f86
Pawel Kubik <p.kubik.pl@gmail.com>
2026-01-30 18:37:04 +0100

    Fix init

--- src/paw_viewer/__init__.py
+++ src/paw_viewer/__init__.py
@@ -1,2 +1,8 @@
-from paw_viewer.viewer import show_video_array
-import paw_viewer.io
+from paw_viewer.viewer import show_video_array, show_video_arrays
+from paw_viewer import io
+
+__all__ = [
+    "show_video_array",
+    "show_video_arrays",
+    "io",
+]
